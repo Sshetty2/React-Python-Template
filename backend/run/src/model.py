f4573fdd5ebf1849d596aa1e4a65ceb7636ee59d
Sachit Shetty @_@ Sonny <sachit.shetty@gmail.com>
2019-04-09 20:37:10 -0400

    and prints in backend

--- backend/run/src/model.py
+++ backend/run/src/model.py
@@ -28,11 +28,14 @@ def validate_pw(useridobj):
     print('endpoint hit')
     try: 
         user_object = set_user_object(userid)
+        print('user object set')
     except:
         return "username error"
+        print('username error')
     pass_hash = user_object.pass_hash
     if user_object.check_password(pass_hash, password):
         return "success"
+        print('success')
     return "password error"
 
 def create_new_user(useridobj):
