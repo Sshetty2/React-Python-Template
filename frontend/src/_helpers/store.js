4f0abb3adbd83648d18d5e34f41d0d433c6ebded
Sachit Shetty @_@ Sonny <sachit.shetty@gmail.com>
2019-04-09 20:37:20 -0400

    remove logger middleware

--- frontend/src/_helpers/store.js
+++ frontend/src/_helpers/store.js
@@ -3,8 +3,8 @@ import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import rootReducer from '../_reducers';
 
-const loggerMiddleware = createLogger();
-const middleware = [thunkMiddleware, loggerMiddleware]
+//const loggerMiddleware = createLogger();
+const middleware = [thunkMiddleware]
 const enhancers = [];
 const initialState = {};
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
